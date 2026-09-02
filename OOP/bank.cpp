f3dcb04d8c6f4415bd890ca3b12c80b6e7d81423
itlab <itlab@nu>
2026-09-02 08:17:54 +0000

    2.09.2026

---
+++ OOP/bank.cpp
@@ -0,0 +1,34 @@
+#include <bits/types/error_t.h>
+#include <iostream>
+#include <ostream>
+#include <string>
+using namespace std;
+class BankAccount {
+  public:    
+    string fname;
+    string lname;
+    long long accountno;
+    int balance;
+};
+
+int main(){
+  char io = 'y';
+  BankAccount n;
+  while (io=='y') {
+    cout<<"Enter the bank account details."<<endl;
+    cout<<"Name: ";
+    cin>>n.fname>>n.lname;
+    cout<<"Account No.: ";
+    cin>>n.accountno;
+    cout<<"Balance: ";
+    cin>>n.balance;
+
+    cout<<"Bank account details: "<<endl;
+    cout<<"Name: "<<n.fname<<' '<<n.lname<<endl;
+    cout<<"Account No: "<<n.accountno<<endl;
+    cout<<"Balance: "<<n.balance<<endl;
+  
+    cout<<"Do you want to enter another record? (y/n): ";
+    cin>>io;
+  }
+}
